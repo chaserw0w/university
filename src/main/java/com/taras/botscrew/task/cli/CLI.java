3563579b9ab6000fc74fa9575879b5b7171e9591
Taras Kurenda <chaserw0w.ti@gmail.com>
2023-09-14 18:07:48 +0300

    configured H2 DB properly and made some refactoring

--- src/main/java/com/taras/botscrew/task/cli/CLI.java
+++ src/main/java/com/taras/botscrew/task/cli/CLI.java
@@ -1,5 +1,6 @@
 package com.taras.botscrew.task.cli;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
+@Slf4j
 public class CLI implements CommandLineRunner {
     @Autowired
     private List<Command> commands;
@@ -42,7 +44,7 @@ public class CLI implements CommandLineRunner {
                             () -> System.out.println("Command not found")
                     );
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    log.error("Exception occurred: " + e.getMessage());
                 }
             }
 
